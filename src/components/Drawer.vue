861add94f056ab0c054ae78f6e2948535e83d6d3
KALIZEI611 <mih80852@gmail.com>
2025-10-22 23:49:42 +0300

    Drawer and search

---
+++ src/components/Drawer.vue
@@ -0,0 +1,28 @@
+<script setup >
+import CartItemList from './CartItemList.vue';
+import DrawerHeader from './DrawerHeader.vue';
+
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+    <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+        <DrawerHeader />
+        <CartItemList />
+        <div class="flex flex-col gap-4 mt-7">
+            <div class="flex gap-2">
+                <span>Итого:</span>
+                <div class="flex-1 border-b border-dashed"></div>
+                <span class="font-bold">12000 руб.</span>
+            </div>
+
+            <div class="flex gap-2">
+                <span>Налог 5%:</span>
+                <div class="flex-1 border-b border-dashed"></div>
+                <span class="font-bold">900 руб.</span>
+            </div>
+        
+            <button disabled="" class="mt-7 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 cursor-pointer hover:bg-lime-600 active:bg-lime-700">Оформить заказ</button>
+        </div>
+    </div>
+</template>
